c8803203e436975feafa7c13300e9cf2b1e125c5
MarkPaulin <paulin.mark@googlemail.com>
2019-08-05 20:43:33 +0100

    LGD data in JSON
    Signed-off-by: MarkPaulin <paulin.mark@googlemail.com>

--- cleaning code/cleaning_mye2018.R
+++ cleaning code/cleaning_mye2018.R
@@ -16,3 +16,18 @@ mye_ni <- mye_raw %>%
 nrow(mye_ni) == (19 * 2 * 48)
 
 write(toJSON(mye_ni), '../data/mye_ni.json')
+
+mye_lgd <- mye_raw %>% 
+  mutate(area_prefix = str_sub(area_code, end = 3)) %>% 
+  filter(year >= 2001) %>% 
+  filter(area_prefix == 'N92' | area_prefix == 'N09') %>% 
+  filter(gender != 'All persons') %>% 
+  mutate(gender = if_else(gender == 'Females', 'F', 'M')) %>%
+  mutate(area_name = str_to_lower(area_name)) %>% 
+  group_by(year, area_name, gender, age_5) %>% 
+  summarise(count = sum(MYE))
+
+# Check sum
+nrow(mye_lgd) == (12 * 19 * 2 * 18)
+
+write(toJSON(mye_lgd), '../data/mye_lgd.json')
